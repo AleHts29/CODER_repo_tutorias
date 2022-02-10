3dc787c530c2165ecc40cb4bb4d39e0a747b05e8
AleHts29 <ahuertas@fi.uba.ar>
2022-02-10 11:47:57 -0300

    desafio Clase - Crear un algoritmo simple

---
+++ Clase_01-Desafio-clase/index.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su Apelllido");
+let edad = parseInt(prompt("Ingrese su nombre"));
+
+alert(`Hola ${nombre} - ${apellido}, tu edad es: ${edad}`);
