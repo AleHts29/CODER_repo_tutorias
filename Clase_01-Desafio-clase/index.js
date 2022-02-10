7b31ddbb6309cff5fa132651d85fdd8795db673e
AleHts29 <ahuertas@fi.uba.ar>
2022-02-10 12:07:34 -0300

    desafio Clase - Crear un algoritmo simple

--- Clase_01-Desafio-clase/index.js
+++ Clase_01-Desafio-clase/index.js
@@ -1,5 +1,13 @@
+// Pedir nombre mediante prompt y mostrarlo en consola junto con algún texto de saludo. Ejemplo:  ¡Hola, Juan!
 let nombre = prompt("Ingrese su nombre");
 let apellido = prompt("Ingrese su Apelllido");
 let edad = parseInt(prompt("Ingrese su nombre"));
 
 alert(`Hola ${nombre} - ${apellido}, tu edad es: ${edad}`);
+
+// Pedir un número mediante prompt, parsearlo, sumarlo a otro que se encuentre almacenado en una variable y luego mostrar el resultado en consola.
+let numero = parseInt(
+  prompt("Ingrese un numero, el cual sera sumado a la edad que ingreso")
+);
+let resultado = edad + numero;
+console.log(`El resultado de la suma de "edad + numero" ${resultado}`);
